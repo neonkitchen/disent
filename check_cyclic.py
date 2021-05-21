1565adadeb64f5189727ae54fa9d2d7fa3f6a0e0
Nathan Michlo <NathanJMichlo@gmail.com>
2021-05-21 04:32:07 +0200

    kind of working

--- check_cyclic.py
+++ check_cyclic.py
@@ -24,9 +24,14 @@
 
 
 import ast
+import dataclasses
+import re
+import string
 from collections import namedtuple
+from functools import lru_cache
 from glob import glob
 from typing import List
+from typing import Optional
 from typing import Tuple
 
 
@@ -48,7 +53,7 @@ def ast_parse(file):
         for child in ast.iter_child_nodes(node):
             child.parent = node
     # done
-    return root
+    return root, data.splitlines()
 
 
 def iter_parents(node):
@@ -62,8 +67,19 @@ def iter_parents(node):
 # ========================================================================= #
 
 
-Import = namedtuple('Import', 'module name')
-Module = namedtuple('Module', 'module full_module file imports scoped_imports')
+@dataclasses.dataclass
+class Import(object):
+    module: str
+    name: Optional[str]
+
+
+@dataclasses.dataclass
+class Module(object):
+    module: str
+    full_module: str
+    file: str
+    imports: List[Import]
+    scoped_imports: List[Import]
 
 
 # ========================================================================= #
@@ -78,8 +94,34 @@ def in_module_scope(node):
     return True
 
 
+@lru_cache()
+def file_to_module_str(file, remove_init=False):
+    assert file.endswith('.py')
+    module = file.replace('/', '.')[:-len('.py')]
+    # assert all(str.isidentifier(token) for token in module.split('.'))
+    # remove __init__
+    if remove_init:
+        if module.endswith('.__init__'):
+            module = module[:-len('.__init__')]
+    return module
+
+
+def make_from_import(file, data, node, module, name):
+    # get line
+    line = re.sub(' +', ' ', data[node.lineno - 1].strip())
+    assert line.startswith('from ')
+    line = line[len('from '):]
+    assert line[0] not in string.whitespace
+    # check if relative
+    if line[0] == '.':
+        module = '.'.join(file_to_module_str(file, remove_init=False).split('.')[:-1] + [module])
+    return Import(module=module, name=name)
+
+
 def parse_imports(file) -> Tuple[List[Import], List[Import]]:
     imports, scoped = [], []
+    # parse
+    root, data = ast_parse(file)
     # visitor
     class Visitor(ast.NodeVisitor):
         def visit_Import(self, node):
@@ -87,24 +129,21 @@ def parse_imports(file) -> Tuple[List[Import], List[Import]]:
                 self._add(node, Import(module=name.name, name=None))
         def visit_ImportFrom(self, node):
             for name in node.names:
-                self._add(node, Import(module=node.module, name=name.name))
+                self._add(node, make_from_import(file=file, data=data, node=node, module=node.module, name=name.name))
         def _add(self, node, imp):
             (imports if in_module_scope(node) else scoped).append(imp)
     # visit
-    Visitor().visit(ast_parse(file))
+    Visitor().visit(root)
     # done
     return imports, scoped
 
 
 def parse_module(file) -> Module:
     imports, scoped_imports = parse_imports(file)
-    # get module strings
-    full_module = file.replace('/', '.')[:-len('.py')]
-    module = full_module[:-len('.__init__')] if full_module.endswith('.__init__') else full_module
     # make module
     return Module(
-        module=module,
-        full_module=full_module,
+        module=file_to_module_str(file, remove_init=True),
+        full_module=file_to_module_str(file, remove_init=False),
         file=file,
         imports=imports,
         scoped_imports=scoped_imports,
@@ -127,20 +166,25 @@ def glob_parse_modules(glob_path='**/*.py', recursive=True):
 
 if __name__ == '__main__':
 
-    modules = glob_parse_modules('disent/frameworks/ae/**/*.py')
+    modules = glob_parse_modules('**/*.py')
 
     import networkx as nx
 
-    G = nx.Graph()
-    for module in modules.values():
-        print(module.module)
-        G.add_node(module.module)
-        for imp in module.imports:
-            print('    ', imp.module)
-            G.add_edge(module.module, imp.module)
-    H = nx.DiGraph(G)
+    def can_show_module(name):
+        return (name.startswith('disent.frameworks.ae') or name.startswith('disent.frameworks.vae')) # and 'experimental' not in name
+    def can_show_import(name):
+        return (name.startswith('disent.frameworks.ae') or name.startswith('disent.frameworks.vae')) # and 'experimental' not in name
 
-    nx.draw(G, with_labels=True, font_weight='bold')
+    G = nx.DiGraph()
+    for module in modules.values():
+        # G.add_node(module.module)
+        for imp in module.imports:
+            # G.add_node(imp.module)
+            if can_show_module(module.module) and can_show_import(imp.module):
+                G.add_edge(imp.module, module.module)
+
+    fig, ax = plt.subplots(figsize=(15, 15))
+    nx.draw_kamada_kawai(G, with_labels=True, font_weight='bold', ax=ax)
     plt.show()
 
 
